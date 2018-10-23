88e1205ffcbbcbd365d4da4e1ba5c309272ebc57
mik <mklimoff222@gmail.com>
2018-10-24 00:44:13 +0400

    added ternjs. config

--- src/App.js
+++ src/App.js
@@ -9,17 +9,19 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 // local deps
-import logo from './logo.svg';
 import './App.css';
-import { simpleAction } from './actions/simpleAction'
+import { simpleAction } from './actions/simpleAction';
 
 const mapDispatchToProps = dispatch => ({
- simpleAction: () => dispatch(simpleAction())
-})
+    simpleAction: () => dispatch(simpleAction())
+});
 
 const mapStateToProps = state => ({
- ...state
-})
+    ...state
+});
+
+
+
 
 class App extends Component {
     simpleAction = (event) => {
@@ -27,27 +29,17 @@ class App extends Component {
     }
     render() {
         return (
-                <div className="App">
-                <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-                >
-                Learn React
-            </a>
-                </header>
+            <div className="App">
+              <header>
+                <div>Hello world</div>
+                <p>Click on this button, for the sake of clicking</p>
                 <button onClick={this.simpleAction}>Test redux action</button>
                 <pre>
-                {JSON.stringify(this.props)}
+                  {JSON.stringify(this.props)}
                 </pre>
-                </div>
-                
+              </header>
+            </div>
+            
         );
     }
 }
